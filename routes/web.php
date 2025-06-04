7717bb6bbfea295f8b51f3766ca6259f5e8f650b
Faris Rizky Ramadhan <faris.7826@gmail.com>
2025-06-04 18:07:31 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BangunRuangController;
 
-Route::get('/bangun-ruang', [BangunRuangController::class, 'index'])->name('show');
-Route::get('/bangun-ruang/create', [BangunRuangController::class, 'create'])->name('create');
-Route::post('/bangun-ruang/store', [BangunRuangController::class, 'store'])->name('store');
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-Route::get('/bangun-ruang', [BangunRuangController::class, 'index'])->name('show');
-Route::delete('/bangun-ruang/{id}', [BangunRuangController::class, 'destroy'])->name('destroy');
+require __DIR__.'/auth.php';
